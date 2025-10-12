b8f714152bacf01d2734a5c526ccd6a3ea59c375
Hexagon Digital Services <hexagonsservices@gmail.com>
2025-10-12 18:06:22 +0530

    Update Movies.jsx

--- frontend/src/components/Movies.jsx
+++ frontend/src/components/Movies.jsx
@@ -3,8 +3,5 @@
         @import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&family=Pacifico&display=swap');
       `}</style>
 
-  <span
-    id={`movie-title-${m.id}`}
-    style={{ fontFamily: "'Pacifico', cursive" }}
-  ></span>
+
 </>;
